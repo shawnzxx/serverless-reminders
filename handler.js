29cdcf542f0becb6823cb482ffc4e8b8c5df53ca
Shawn Zhang <shawn.zhang@razerzone.com>
2018-04-08 11:20:13 +0800

    add in

--- handler.js
+++ handler.js
@@ -1,16 +1,85 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.sendReminderDaily = (event, context, callback) => {
 
-  callback(null, response);
+    let AWS = require('aws-sdk');
+    AWS.config.update({region:'ap-southeast-1'});
+    let ses = new AWS.SES();
+    let fs = require('fs');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    let emailHtml = fs.readFileSync('./dailyReminder.html', 'utf-8');
+
+    let toAndFromAddress = 'shawn.zhang@razer.com';
+    let params = {
+        Destination: {
+            ToAddresses: [toAndFromAddress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: emailHtml
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: "Remember to continue helping the Woof Garden in your Pluralsight course!"
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "Woof Garden Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAddress],
+        Source: toAndFromAddress,
+    };
+
+    ses.sendEmail(params, function(err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack);
+        // successful response
+        else callback(null, data);
+    });
+};
+
+module.exports.sendReminderWeekend = (event, context, callback) => {
+
+    let AWS = require('aws-sdk');
+    AWS.config.update({region:'us-east-1'});
+    let ses = new AWS.SES();
+    let fs = require('fs');
+
+    let emailHtml = fs.readFileSync('./weekendReminder.html', 'utf-8');
+
+    let toAndFromAddress = 'shawn.zhang@razer.com';
+    let params = {
+        Destination: {
+            ToAddresses: [toAndFromAddress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: emailHtml
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: "Here's a weekend Remember that puppies are adorable!!"
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "Woof Garden Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAddress],
+        Source: toAndFromAddress,
+    };
+
+    ses.sendEmail(params, function(err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack);
+        // successful response
+        else callback(null, data);
+    });
 };
